947f222ff9064ecafb419e40cca7d709ca7c9d4a
Atmajaaa <atmajap74@gmail.com>
2021-12-15 17:42:23 +0530

    Add Registration

---
+++ Registration.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Concert_ticket_booking_system_final
+{
+    partial class Registration
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.usernamePanel = new System.Windows.Forms.Panel();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.locklogo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.confirmpasswordTextbox = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.registerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordPanel = new System.Windows.Forms.Panel();
+            this.confirmPasswordPanel = new System.Windows.Forms.Panel();
+            this.errorLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locklogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, -27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(520, 555);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.BackColor = System.Drawing.SystemColors.Menu;
+            this.passwordTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordTextbox.ForeColor = System.Drawing.Color.Black;
+            this.passwordTextbox.Location = new System.Drawing.Point(620, 220);
+            this.passwordTextbox.MaxLength = 8;
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PlaceholderText = "Password";
+            this.passwordTextbox.Size = new System.Drawing.Size(261, 16);
+            this.passwordTextbox.TabIndex = 30;
+            this.passwordTextbox.UseSystemPasswordChar = true;
+            // 
+            // usernamePanel
+            // 
+            this.usernamePanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.usernamePanel.Location = new System.Drawing.Point(617, 173);
+            this.usernamePanel.Name = "usernamePanel";
+            this.usernamePanel.Size = new System.Drawing.Size(263, 2);
+            this.usernamePanel.TabIndex = 25;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTextBox.ForeColor = System.Drawing.Color.Black;
+            this.usernameTextBox.Location = new System.Drawing.Point(617, 149);
+            this.usernameTextBox.MaxLength = 20;
+            this.usernameTextBox.Multiline = true;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.PlaceholderText = "Username";
+            this.usernameTextBox.Size = new System.Drawing.Size(261, 23);
+            this.usernameTextBox.TabIndex = 28;
+            // 
+            // locklogo
+            // 
+            this.locklogo.Image = ((System.Drawing.Image)(resources.GetObject("locklogo.Image")));
+            this.locklogo.Location = new System.Drawing.Point(572, 215);
+            this.locklogo.Name = "locklogo";
+            this.locklogo.Size = new System.Drawing.Size(33, 27);
+            this.locklogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.locklogo.TabIndex = 27;
+            this.locklogo.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(572, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.registerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.registerBtn.ForeColor = System.Drawing.Color.AliceBlue;
+            this.registerBtn.Location = new System.Drawing.Point(577, 355);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(306, 57);
+            this.registerBtn.TabIndex = 23;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            this.registerBtn.ForeColorChanged += new System.EventHandler(this.registerBtn_Click);
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // confirmpasswordTextbox
+            // 
+            this.confirmpasswordTextbox.BackColor = System.Drawing.SystemColors.Menu;
+            this.confirmpasswordTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.confirmpasswordTextbox.ForeColor = System.Drawing.Color.Black;
+            this.confirmpasswordTextbox.Location = new System.Drawing.Point(621, 285);
+            this.confirmpasswordTextbox.MaxLength = 8;
+            this.confirmpasswordTextbox.Name = "confirmpasswordTextbox";
+            this.confirmpasswordTextbox.PlaceholderText = "Confirm Password";
+            this.confirmpasswordTextbox.Size = new System.Drawing.Size(261, 16);
+            this.confirmpasswordTextbox.TabIndex = 33;
+            this.confirmpasswordTextbox.UseSystemPasswordChar = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(573, 280);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(33, 27);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 31;
+            this.pictureBox3.TabStop = false;
+            // 
+            // registerLabel
+            // 
+            this.registerLabel.AutoSize = true;
+            this.registerLabel.Font = new System.Drawing.Font("Britannic Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.registerLabel.Location = new System.Drawing.Point(602, 33);
+            this.registerLabel.Name = "registerLabel";
+            this.registerLabel.Size = new System.Drawing.Size(281, 53);
+            this.registerLabel.TabIndex = 34;
+            this.registerLabel.Text = "Registration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(639, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 15);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Already a user? ";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.loginLabel.Location = new System.Drawing.Point(722, 419);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(106, 15);
+            this.loginLabel.TabIndex = 36;
+            this.loginLabel.Text = "Click here to Login";
+            this.loginLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // passwordPanel
+            // 
+            this.passwordPanel.BackColor = System.Drawing.Color.Black;
+            this.passwordPanel.Location = new System.Drawing.Point(620, 240);
+            this.passwordPanel.Name = "passwordPanel";
+            this.passwordPanel.Size = new System.Drawing.Size(263, 2);
+            this.passwordPanel.TabIndex = 26;
+            // 
+            // confirmPasswordPanel
+            // 
+            this.confirmPasswordPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.confirmPasswordPanel.Location = new System.Drawing.Point(621, 306);
+            this.confirmPasswordPanel.Name = "confirmPasswordPanel";
+            this.confirmPasswordPanel.Size = new System.Drawing.Size(263, 2);
+            this.confirmPasswordPanel.TabIndex = 26;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.errorLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.errorLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.errorLabel.Location = new System.Drawing.Point(621, 320);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Padding = new System.Windows.Forms.Padding(10, 5, 25, 5);
+            this.errorLabel.Size = new System.Drawing.Size(35, 23);
+            this.errorLabel.TabIndex = 37;
+            this.errorLabel.Visible = false;
+            // 
+            // Registration
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 485);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.confirmPasswordPanel);
+            this.Controls.Add(this.passwordPanel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.registerLabel);
+            this.Controls.Add(this.confirmpasswordTextbox);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.usernamePanel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.locklogo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Registration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Click Concert";
+            this.Load += new System.EventHandler(this.Registration_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locklogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Panel usernamePanel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.PictureBox locklogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.TextBox confirmpasswordTextbox;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label registerLabel;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel confirmPasswordPanel;
+        private System.Windows.Forms.Panel passwordPanel;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
